3a4ba3a382497a2bb77387908136ea9252a4c81a
czp <zuopingchen@gmail.com>
2021-01-19 13:27:02 +0000

    https

--- main.go
+++ main.go
@@ -28,5 +28,23 @@ func main() {
 	get = plugin.Plugin_get_filecache(get)
 	route.PathPrefix("/get").HandlerFunc(get)
 
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(lib.Conf.HttpPort), route))
+	if lib.Conf.HttpPort>0 && lib.Conf.HttpsPort>0 {
+		go func ()  {
+			log.Fatal(http.ListenAndServe(":"+strconv.Itoa(lib.Conf.HttpPort), route))
+		}()
+		err_https := http.ListenAndServeTLS(":"+strconv.Itoa(lib.Conf.HttpsPort),     lib.Conf.HttpsPublicKeyPath, lib.Conf.HttpsPrivateKeyPath, route)
+		if err_https != nil {
+    	log.Fatal("Web server (HTTPS): ", err_https)
+		}	
+	}else{
+		if lib.Conf.HttpPort>0 {
+			log.Fatal(http.ListenAndServe(":"+strconv.Itoa(lib.Conf.HttpPort), route))
+		}
+		
+		if lib.Conf.HttpsPort>0{
+			log.Fatal(http.ListenAndServeTLS(":"+strconv.Itoa(lib.Conf.HttpsPort),     lib.Conf.HttpsPublicKeyPath, lib.Conf.HttpsPrivateKeyPath, route))
+		}
+	}
+
+	
 }
